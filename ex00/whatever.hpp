e0975ade53bdd1815b1bb90398efd3f5fb1d8d18
Chiara Kappe <chiara@Chiaras-MacBook-Pro.local>
2026-06-11 22:03:14 +0200

    added template function for swapping

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -17,3 +17,11 @@ template <typename T> void swap(T& a, T& b){
     a = b;
     b = temp;
 }
+
+/* Implement the following function templates:
+• swap: Swaps the values of two given parameters. Does not return anything.
+• min: Compares the two values passed as parameters and returns the smallest one. If they are equal, it returns the second one.
+• max: Compares the two values passed as parameters and returns the greatest one. If they are equal, it returns the second one.
+These functions can be called with any type of argument. 
+The only requirement is that the two arguments must have the same type and must support all the comparison operators.
+ */
